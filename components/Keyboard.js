f3207e7d92663093955ce6811c2c52029c659ef0
Albin Hagström <albin.hagstrom@weahead.se>
2018-01-09 16:07:38 +0100

    increase intensity on every keypress

--- components/Keyboard.js
+++ components/Keyboard.js
@@ -4,51 +4,51 @@ import Boom from './Boom';
 
 import { compose, withState, withHandlers, lifecycle } from 'recompose';
 
-const Keyboard = (props) => (
-  props.armedKey === props.lastKeyPressed ?
-    <Boom />
-  :
-    <div className="wrapper" onKeyDown={props.onKeyDown} tabIndex="0">
-      <div className="keyboard">
-        {keys.map((row, i) => <KeyboardRow activeKeys={props.keysPressed} key={i} keyObj={keys[i]} />)}
-      </div>
-      <style jsx>{`
-          .wrapper {
-              height: 100vh;
-              display: flex;
-              align-items: center;
-          }
-          .keyboard {
-              text-align: center;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              align-items: center;
-              width: 100%;
-          }
-      `}</style>
-    </div>
-);
+const Keyboard = props =>
+    props.armedKey === props.lastKeyPressed ? (
+        <Boom />
+    ) : (
+        <div className="wrapper" onKeyDown={props.onKeyDown} tabIndex="0">
+            <div className="keyboard">
+                {keys.map((row, i) => <KeyboardRow activeKeys={props.keysPressed} key={i} keyObj={keys[i]} />)}
+            </div>
+            <style jsx>{`
+                .wrapper {
+                    height: 100vh;
+                    display: flex;
+                    align-items: center;
+                }
+                .keyboard {
+                    text-align: center;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    width: 100%;
+                }
+            `}</style>
+        </div>
+    );
 
 const enhance = compose(
-  withState('lastKeyPressed', 'setLastKey', 0),
-  withState('keysPressed', 'setPressedKeys', []),
-  withState('armedKey', 'setArmedKey', 0),
-  withHandlers({
-    onKeyDown: props => event => {
-      props.setLastKey(event.keyCode);
-      props.setPressedKeys([...props.keysPressed, event.keyCode]);
-    },
-    setArmed: props => () => {
-      const randomNum = Math.floor(Math.random() * codes.length);
-      props.setArmedKey(codes[randomNum]);
-    },
-  }),
-  lifecycle({
-    componentDidMount() {
-      this.props.setArmed();
-    }
-  })
+    withState('lastKeyPressed', 'setLastKey', 0),
+    withState('keysPressed', 'setPressedKeys', []),
+    withState('armedKey', 'setArmedKey', 0),
+    withHandlers({
+        onKeyDown: props => event => {
+            props.setLastKey(event.keyCode);
+            props.setPressedKeys([...props.keysPressed, event.keyCode]);
+        },
+        setArmed: props => () => {
+            const randomNum = Math.floor(Math.random() * codes.length);
+            props.setArmedKey(codes[randomNum]);
+        }
+    }),
+    lifecycle({
+        componentDidMount() {
+            this.props.setArmed();
+        }
+    })
 );
 
 export default enhance(Keyboard);
--- components/KeyboardRow.js
+++ components/KeyboardRow.js
@@ -1,35 +1,50 @@
-const KeyboardRow = props => (
-  <div className="keyboardRow">
-    {props.keyObj.map((obj, i) =>
-        <span key={i} className={props.activeKeys.includes(obj.code) ? "key active" : "key"}>
-        {obj.key}
-        </span>
-    )}
-    <style jsx>{`
-      .keyboardRow {
-        text-align: center;
-        display: flex;
-        justify-content: center;
-        width: 100%;
-      }
-      .key{
-        padding: 20px 30px;
-        font-size: 24px;
-        background: #e7e7e7;
-        margin: 5px;
-        border-radius: 5px;
-        color: #212121;
-        box-shadow: rgba(0, 0, 0, 0.23) 3px 5px 5px 0px;
-        text-transform: uppercase;
-        transition: all 0.1s ease;
-      }
-      .active{
-        background: #FF7043;
-        color: #F5F5F5;
-        box-shadow: rgba(0, 0, 0, 0.23) 0px 3px 7px 0px;
-      }
-    `}</style>
-  </div>
-);
+import { codes } from './keyCodes';
+
+const KeyboardRow = props => {
+    const intensity = (codes.length - props.activeKeys.length) / 100; //ALGORITM MÅSTE FIXASS ASAAPPPPP
+    return (
+        <div className="keyboardRow">
+            {props.keyObj.map((obj, i) => (
+                <span key={i} className={props.activeKeys.includes(obj.code) ? 'key active' : 'key'}>
+                    {obj.key}
+                </span>
+            ))}
+            <style jsx>{`
+                .keyboardRow {
+                    text-align: center;
+                    display: flex;
+                    justify-content: center;
+                    width: 100%;
+                }
+                .key {
+                    padding: 20px 30px;
+                    font-size: 24px;
+                    background: #e7e7e7;
+                    margin: 5px;
+                    border-radius: 5px;
+                    color: #212121;
+                    box-shadow: rgba(0, 0, 0, 0.23) 3px 5px 5px 0px;
+                    text-transform: uppercase;
+                    transition: all 0.1s ease;
+                }
+                .active {
+                    color: #f5f5f5;
+                    box-shadow: rgba(0, 0, 0, 0.23) 0px 3px 7px 0px;
+                    animation: wiggle ${intensity}s infinite alternate;
+                }
+                @keyframes wiggle {
+                    from {
+                        transform: rotate(2deg);
+                        background: #ff7043;
+                    }
+                    to {
+                        transform: rotate(-2deg);
+                        background: #ff5722;
+                    }
+                }
+            `}</style>
+        </div>
+    );
+};
 
 export default KeyboardRow;
